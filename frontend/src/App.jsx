d77e24057c8dd3126419067efa3f7e81cfb318ef
Hrishik1033 <b25me1033@iitj.ac.in>
2026-03-08 17:59:16 +0530

    Updated:VERDICT(ver2)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
 export default App*/ 
 import React, { useEffect, useState } from 'react'
 import { io } from 'socket.io-client'
-
+import './App.css'
 const socket = io()
 
 const App = () => {
